f94005a813f0df12903117acece9a9781b9c74e7
hRJ06 <hindolroy.2306@gmail.com>
2023-10-13 00:18:03 +0530

    Added GFG POTD

---
+++ GFG/Floor In BST.cpp
@@ -0,0 +1,16 @@
+class Solution{
+
+public:
+    int floor(Node* root, int x) {
+        int ans = -1;
+        while(root) {
+            if(root->data == x) return x;
+            else if(root->data > x) root = root->left;
+            else {
+                ans = root->data;
+                root = root->right;
+            }
+        }
+        return ans;
+    }
+};
