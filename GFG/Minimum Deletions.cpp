2d91886a2ea0a513ec421180fac9b22a6b20da32
hRJ06 <hindolroy.2306@gmail.com>
2023-10-27 00:51:39 +0530

    Added GFG POTD

---
+++ GFG/Minimum Deletions.cpp
@@ -0,0 +1,24 @@
+class Solution{
+    private:
+    int f(string s,string t) {
+        int n = s.length(), m = t.length();
+        vector<vector<int>> dp(n + 1,vector<int>(m + 1,0));
+        for(int i = 1; i<=n; i++) {
+            for(int j = 1; j<=m; j++) {
+                if(s[i - 1] != t[j - 1]) 
+                    dp[i][j] = max(dp[i - 1][j],dp[i][j - 1]);
+                else
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+        }
+        return dp[n][m];
+    }
+  public:
+    int minimumNumberOfDeletions(string s) { 
+        // code here
+        string t = s;
+        reverse(t.begin(),t.end());
+        int lcs = f(s,t);
+        return s.length() - lcs;
+    } 
+};
