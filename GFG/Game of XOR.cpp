4c8169a822a179aa4368465a4718fd69daf68949
hRJ06 <hindolroy.2306@gmail.com>
2023-12-19 00:11:07 +0530

    Added GFG POTD 18.12.2023

---
+++ GFG/Game of XOR.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int gameOfXor(int N , int A[]) {
+        // code here
+        int ans = 0;
+        for(int i = 0; i<N; i++) {
+            if(((i + 1) & 1) and ((N - i) & 1))
+                ans ^= A[i];
+        }
+        return ans;
+    }
+};
