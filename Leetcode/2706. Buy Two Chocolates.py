81103b7b207d74ed3068b936d27d4022060d0ee6
hRJ06 <hindolroy.2306@gmail.com>
2023-12-20 23:32:30 +0530

    Added LeetCode POTD

---
+++ Leetcode/2706. Buy Two Chocolates.py
@@ -0,0 +1,9 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        prices.sort()
+        total = prices[0] + prices[1]
+        if total <= money:
+            return money - total
+        else:
+            return money
+        
