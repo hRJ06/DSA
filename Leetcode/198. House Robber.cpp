c1ef80f4e75709a46e2fd04332387f37266769db
hRJ06 <hindolroy.2306@gmail.com>
2024-01-22 22:13:44 +0530

    Added LeetCode POTD 21.01.2024

---
+++ Leetcode/198. House Robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        int prev1 = nums[0], prev2 = 0;
+        for(int i = 1; i<n; i++) {
+            int take = nums[i];
+            if(i > 1) take += prev2;
+            int notTake = prev1;
+            int cur = max(take,notTake);
+            prev2 = prev1;
+            prev1 = cur;
+        }
+        return prev1;
+    }
+};
