779fec61b7221e1a8d944797c0202ac8517f5c9a
hRJ06 <hindolroy.2306@gmail.com>
2024-01-25 11:33:36 +0530

    Added LeetCode POTD

---
+++ Leetcode/1143. Longest Common Subsequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n, m = len(text1), len(text2)
+        prev = [0] * (m + 1)
+        cur = [0] * (m + 1)
+        for i in range(1,n + 1):
+            for j in range(1,m + 1):
+                if text1[i - 1] != text2[j - 1]:
+                    cur[j] = max(prev[j],cur[j - 1])
+                else:
+                    cur[j] = 1 + prev[j - 1]
+            prev = cur.copy()
+        return prev[m]
