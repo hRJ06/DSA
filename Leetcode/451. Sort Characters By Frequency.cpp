b0989552bb05de2c12999ff2501597af6496de3f
hRJ06 <hindolroy.2306@gmail.com>
2024-02-07 17:51:29 +0530

    Added LeetCode  POTD

---
+++ Leetcode/451. Sort Characters By Frequency.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> mp;
+        for(auto &i : s) mp[i]++;
+        sort(s.begin(),s.end(),[&](char a,char b){
+            if(mp[a] != mp[b]) {
+                return mp[a] > mp[b];
+            } 
+            else {
+                return a < b;
+            }
+        });
+        return s;
+    }
+};
