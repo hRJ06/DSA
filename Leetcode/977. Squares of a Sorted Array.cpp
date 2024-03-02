3c7a67b61810a9ee5f5677066f17980890c6fb5e
hRJ06 <hindolroy.2306@gmail.com>
2024-03-02 18:51:15 +0530

    Added LeetCode POTD

---
+++ Leetcode/977. Squares of a Sorted Array.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> v;
+        for(auto &i : nums) v.push_back(i * i);
+        sort(v.begin(),v.end());
+        return v;
+    }
+};
