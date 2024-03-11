fbb6dcdbe1be5d2ada97b6274adb47202ac8f581
hRJ06 <hindolroy.2306@gmail.com>
2024-03-11 17:25:36 +0530

    Added GFG POTD

---
+++ GFG/Count pairs Sum in matrices.cpp
@@ -0,0 +1,34 @@
+class Solution{
+public:	
+	
+	int countPairs(vector<vector<int>> &mat1, vector<vector<int>> &mat2, int n, int x)
+	{
+	    // Your code goes here
+	    int r1 = 0, c1 = 0;
+	    int r2 = n - 1, c2 = n - 1;
+	    int ans = 0;
+	    while(r1 < n and r2 >= 0) {
+	        int s = mat1[r1][c1] + mat2[r2][c2];
+	        if(s < x) {
+	            c1++;
+	        }
+	        else if(s > x) {
+	            c2--;
+	        }
+	        if(s == x) {
+	            c1++;
+	            c2--;
+	            ans++;
+	        }
+	        if(c1 == n) {
+	            c1 = 0;
+	            r1++;
+	        }
+	        if(c2 == -1) {
+	            c2 = n - 1;
+	            r2--;
+	        }
+	    }
+	    return ans;
+	}
+};
