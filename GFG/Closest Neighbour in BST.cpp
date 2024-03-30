7350f2eb4a4d7f9089d48c29e8c1068d8d3a9603
hRJ06 <hindolroy.2306@gmail.com>
2024-03-31 01:36:04 +0530

    Added GFG POTD

---
+++ GFG/Closest Neighbour in BST.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int findMaxForN(Node* root, int n) {
+        // code here
+        int ans = -1;
+        while(root) {
+            if(root->key <= n) {
+                ans = root->key;
+                root = root->right;
+            }
+            else 
+                root = root->left;
+        }
+        return ans;
+    }
+};
