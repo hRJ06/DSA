261d3d9516857a7a771467ff5ab2d100730584e4
hRJ06 <hindolroy.2306@gmail.com>
2024-04-14 03:05:04 +0530

    Added GFG POTD

---
+++ GFG/Xoring and Clearing.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    void printArr(int n, int arr[]) {
+        // Your code for printing array here
+        for(int i = 0; i<n; i++) cout << arr[i] << " ";
+        cout << "\n";
+    }
+
+    void setToZero(int n, int arr[]) {
+        // Use memset to set arr to zero
+        for(int i = 0; i<n; i++) arr[i] = 0;
+    }
+
+    void xor1ToN(int n, int arr[]) {
+        // Xor arr[i]^i
+        for(int i = 0; i<n; i++) arr[i] ^= i;
+    }
+};
