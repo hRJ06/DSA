567b7ba3dd70a16f20125d6da7a795bb213865fe
hRJ06 <hindolroy.2306@gmail.com>
2024-04-15 02:54:14 +0530

    Added GFG POTD

---
+++ GFG/Count the elements.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    vector<int> countElements(vector<int> &a, vector<int> &b, int n, vector<int> &query,
+                              int q) {
+        // Your code goes here;
+        vector<int> ans;
+        sort(b.begin(), b.end());
+        for(int i = 0; i<q; i++) {
+            int count = upper_bound(b.begin(), b.end(), a[query[i]]) - b.begin();
+            ans.push_back(count);
+        }
+        return ans;
+    }
+};
