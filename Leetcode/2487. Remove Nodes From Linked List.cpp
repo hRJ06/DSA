f12aceb28a53e470524622eaac960a2eb6355191
hRJ06 <hindolroy.2306@gmail.com>
2024-05-06 23:37:04 +0530

    Added LeetCode POTD

---
+++ Leetcode/2487. Remove Nodes From Linked List.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        if(!head) return NULL;
+        ListNode* n = removeNodes(head->next);
+        head->next = n;
+        return head->next != NULL and head->val < head->next->val ? head->next : head;
+    }
+};
