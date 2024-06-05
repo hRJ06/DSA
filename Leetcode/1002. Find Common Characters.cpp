9019d95221fbd5938578d47549607adc22702e8f
hRJ06 <hindolroy.2306@gmail.com>
2024-06-05 21:37:32 +0530

    Added LeetCode POTD

---
+++ Leetcode/1002. Find Common Characters.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector<int> minFreq(26, INT_MAX);
+        for(auto &word : words) {
+            vector<int> freq(26, 0);
+            for(char c : word) {
+                freq[c - 'a']++;
+            }
+            for(int i = 0; i < 26; i++) minFreq[i] = min(minFreq[i], freq[i]);
+        }
+        vector<string> ans;
+        for(int i = 0; i<26; i++) 
+            for(int j = 0; j<minFreq[i]; j++) ans.push_back(string(1, 'a' + i));
+        return ans;
+    }
+};
