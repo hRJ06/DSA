896a36091ca01fdc0d4db1f414a3fdfe50542d9f
hRJ06 <hindolroy.2306@gmail.com>
2024-10-07 10:27:54 +0530

    Added LeetCode POTD

---
+++ Leetcode/2696. Minimum String Length After Removing Substrings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> st;
+        for(auto &i : s) {
+            if(st.empty()) st.push(i);
+            else {
+                if(i == 'B' and st.top() == 'A') st.pop();
+                else if(i == 'D' and st.top() == 'C') st.pop();
+                else st.push(i);
+            }
+        }
+        return st.size();
+    }
+};
