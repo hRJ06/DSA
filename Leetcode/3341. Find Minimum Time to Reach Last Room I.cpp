ccf5484790f89d78f9df94a2b8019841970799bc
hRJ06 <hindolroy.2306@gmail.com>
2025-05-08 01:21:21 +0530

    Added LeetCode POTD

---
+++ Leetcode/3341. Find Minimum Time to Reach Last Room I.cpp
@@ -0,0 +1,29 @@
+typedef pair<int, pair<int,int>> p;
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int m = moveTime.size(), n = moveTime[0].size();
+        priority_queue<p, vector<p>, greater<p>> pq; /* {t, {i, j}} */
+        pq.push({0, {0, 0}});
+        vector<vector<bool>> vis(m, vector<bool>(n, false));
+        vector<int> dir = {-1, 0, 1, 0, -1};
+        while(!pq.empty()) {
+            p top = pq.top();
+            pq.pop();
+            int time = top.first;
+            int x = top.second.first;
+            int y = top.second.second;
+            if(x == m - 1 and y == n - 1) return time;
+            for(int i = 0; i<4; i++) {
+                int nx = x + dir[i];
+                int ny = y + dir[i + 1];
+                if(nx >= 0 and nx < m and ny >= 0 and ny < n and !vis[nx][ny]) {
+                    int t_ng = max(time + 1, moveTime[nx][ny] + 1);
+                    pq.push({t_ng, {nx, ny}});
+                    vis[nx][ny] = true;
+                }
+            }
+        }
+        return -1;
+    }
+};
