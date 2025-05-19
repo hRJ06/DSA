3c664e25330ece9670fc84958ebe83fa9d05531e
hRJ06 <hindolroy.2306@gmail.com>
2025-05-19 23:50:11 +0530

    Added LeetCode POTD

---
+++ Leetcode/3024. Type of Triangle.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        if(nums[0] + nums[1] <= nums[2]) return "none";
+        set<int> s(nums.begin(), nums.end());
+        if(s.size() == 1) return "equilateral";
+        else if(s.size() == 2) return "isosceles";
+        return "scalene";
+    }
+};
